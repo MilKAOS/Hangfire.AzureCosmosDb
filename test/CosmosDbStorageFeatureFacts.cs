9812d778b17588e6d9047d7ec0798e028a17d73d
Mil <Michael.Werlitz@gmz.migros.ch>
2024-10-18 13:48:35 +0200

    feat: add tests for JobStorageFeatures.JobQueueProperty

---
+++ test/CosmosDbStorageFeatureFacts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using Hangfire.Azure.Tests.Fixtures;
+using Hangfire.Common;
+using Hangfire.Storage;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Hangfire.Azure.Tests;
+
+public class CosmosDbStorageFeatureFacts : IClassFixture<ContainerFixture>
+{
+    private ContainerFixture ContainerFixture { get; }
+
+    private CosmosDbStorage Storage { get; }
+
+    public CosmosDbStorageFeatureFacts(ContainerFixture containerFixture, ITestOutputHelper testOutputHelper)
+    {
+        ContainerFixture = containerFixture;
+        Storage = containerFixture.Storage;
+
+        ContainerFixture.SetupLogger(testOutputHelper);
+    }    
+
+    [Fact]
+    public void AddOrUpdate_ThrowsAnException_WhenJobQueueIsSet_WhenStorageFeaturesDoesNotSupportIt()
+    {
+        // Arrange
+        Storage.Features = new ReadOnlyDictionary<string, bool>(new Dictionary<string, bool>
+        {
+            { JobStorageFeatures.JobQueueProperty, false },
+        });
+
+        Job job = Job.FromExpression(() => Method(), "some-queue");
+        RecurringJobManager manager = new (Storage);
+
+        // Act & Assert
+        Assert.Throws<NotSupportedException>(() => manager.AddOrUpdate("recurring-job-id", job, Cron.Minutely()));
+    }
+
+    [Fact]
+    public void AddOrUpdate_NoException_WhenJobQueueIsSet_WhenStorageFeaturesDoesSupportIt()
+    {
+        // Arrange
+        Storage.Features = new ReadOnlyDictionary<string, bool>(new Dictionary<string, bool>
+        {
+            { JobStorageFeatures.JobQueueProperty, true },
+        });
+
+        Job job = Job.FromExpression(() => Method(), "some-queue");
+        RecurringJobManager manager = new (Storage);
+
+        // Act & Assert
+        try
+        {
+            manager.AddOrUpdate("recurring-job-id", job, Cron.Minutely());
+        }
+        catch
+        {
+            Assert.Fail("Expected no exception for feature JobStorageFeatures.JobQueueProperty set to true.");
+        }
+    }
+
+    [SuppressMessage("Usage", "xUnit1013:Public method should be marked as test")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    public static void Method() { }
+}
